efa6569abe5b4c9647d6669cd355d0f3b1d16441
dpbpnp <49863807+dpbnnp@users.noreply.github.com>
2020-10-13 21:42:07 +0800

    Update 排序矩阵查找.cpp

--- divide and conquer/排序矩阵查找.cpp
+++ divide and conquer/排序矩阵查找.cpp
@@ -19,7 +19,7 @@
 
 给定 target = 20，返回 false。
 
-*/
+/*
 when it comes to problem of sorting the sequence, it is easy to think about binary search,
 which couble sovled by the way called dividinf and conquering. but in this situation ,it may
 not work out.beacause it is a matrix. when we use the binary search on the row, we may ignore 
@@ -27,53 +27,36 @@ the column, vice versa.
 up to now, the best way i have is find the row which first number is lower than the target, and
 use binary search on the column.
 my solution is as below. but it has some bug, and i can't find them out.
+*/
 /*
 analyse
 
-Line 1038: Char 9: runtime error: reference binding to null pointer of type 'std::vector<int, std::allocator<int> >' (stl_vector.h)
-SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/stl_vector.h:1043:9
+my code as below shows that i still don't kown how to find the common characteristic of the problem,
+some special situation could be contadined in the whole solution ,but i didn't had it well. I should 
+review the binary search againe.
 
 */
 
-bool searchMatrix(vector<vector<int> >& matrix, int target) {
-	int i;
+  bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int i;
 	int j;
-	if(matrix.size()<=0)
+    if(matrix.size()==0)
+    {
+        return false;
+    }
+	if(matrix.size()==1&&matrix[0].size()<=0)
 	{
 		return false;
 	}
-	if(matrix.size()==1)
-	{
-		j=matrix[0].size()/2;
-			int low=0;
-			int high=matrix[0].size()-1;
-			//printf("i:%d\t j:%d\n",i,j) ;
-			
-			while(j>0&&j<matrix[0].size()-1&&low<=high&&j>=low&&j<=high)
-			{
-				if(matrix[j]==target)
-				{
-					return true;
-				}
-				else if(matrix[j]>target)
-				{
-					high=j-1;
-					j=(low+j)/2;
-				}
-				else
-				{
-					low=j+1;
-					j=(high+j)/2;
-				}
-			}
-			return false; 
-	}
+    if(matrix[0][0]==target) 
+			return true;
 	if(target<matrix[0][0]||target>matrix[matrix.size()-1][matrix[0].size()-1])
 		return false;
 	i=matrix.size()/2;
-	for(i=0;i<matrix.size()-1;i++)
+	for(i=0;i<matrix.size();i++)
 	{
-		
+		if(matrix[i][0]==target) 
+			return true;
 		if(matrix[i][0]<target)
 		{
 			j=matrix[0].size()/2;
@@ -81,7 +64,7 @@ bool searchMatrix(vector<vector<int> >& matrix, int target) {
 			int high=matrix[0].size()-1;
 			//printf("i:%d\t j:%d\n",i,j) ;
 			
-			while(j>0&&j<matrix[0].size()-1&&low<=high&&j>=low&&j<=high)
+			while(j>0&&j<=matrix[0].size()-1&&low<=high&&high<matrix[0].size()&&j>=low&&j<=high)
 			{
 				if(matrix[i][j]==target)
 				{
@@ -95,7 +78,7 @@ bool searchMatrix(vector<vector<int> >& matrix, int target) {
 				else
 				{
 					low=j+1;
-					j=(high+j)/2;
+					j=(high+j+1)/2;
 				}
 			}
 		} 
@@ -105,4 +88,4 @@ bool searchMatrix(vector<vector<int> >& matrix, int target) {
 		} 
 	}
 	return false;
-}
+    }
