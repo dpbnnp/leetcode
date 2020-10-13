871eb00e8dce3a5073221ca2eee01dd0cc96fce9
dpbpnp <49863807+dpbnnp@users.noreply.github.com>
2020-10-13 21:03:13 +0800

    Create 排序矩阵查找.cpp

---
+++ divide and conquer/排序矩阵查找.cpp
@@ -0,0 +1,108 @@
+/*
+面试题 10.09. 排序矩阵查找
+
+给定M×N矩阵，每一行、每一列都按升序排列，请编写代码找出某元素。
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+*/
+when it comes to problem of sorting the sequence, it is easy to think about binary search,
+which couble sovled by the way called dividinf and conquering. but in this situation ,it may
+not work out.beacause it is a matrix. when we use the binary search on the row, we may ignore 
+the column, vice versa.
+up to now, the best way i have is find the row which first number is lower than the target, and
+use binary search on the column.
+my solution is as below. but it has some bug, and i can't find them out.
+/*
+analyse
+
+Line 1038: Char 9: runtime error: reference binding to null pointer of type 'std::vector<int, std::allocator<int> >' (stl_vector.h)
+SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/stl_vector.h:1043:9
+
+*/
+
+bool searchMatrix(vector<vector<int> >& matrix, int target) {
+	int i;
+	int j;
+	if(matrix.size()<=0)
+	{
+		return false;
+	}
+	if(matrix.size()==1)
+	{
+		j=matrix[0].size()/2;
+			int low=0;
+			int high=matrix[0].size()-1;
+			//printf("i:%d\t j:%d\n",i,j) ;
+			
+			while(j>0&&j<matrix[0].size()-1&&low<=high&&j>=low&&j<=high)
+			{
+				if(matrix[j]==target)
+				{
+					return true;
+				}
+				else if(matrix[j]>target)
+				{
+					high=j-1;
+					j=(low+j)/2;
+				}
+				else
+				{
+					low=j+1;
+					j=(high+j)/2;
+				}
+			}
+			return false; 
+	}
+	if(target<matrix[0][0]||target>matrix[matrix.size()-1][matrix[0].size()-1])
+		return false;
+	i=matrix.size()/2;
+	for(i=0;i<matrix.size()-1;i++)
+	{
+		
+		if(matrix[i][0]<target)
+		{
+			j=matrix[0].size()/2;
+			int low=0;
+			int high=matrix[0].size()-1;
+			//printf("i:%d\t j:%d\n",i,j) ;
+			
+			while(j>0&&j<matrix[0].size()-1&&low<=high&&j>=low&&j<=high)
+			{
+				if(matrix[i][j]==target)
+				{
+					return true;
+				}
+				else if(matrix[i][j]>target)
+				{
+					high=j-1;
+					j=(low+j)/2;
+				}
+				else
+				{
+					low=j+1;
+					j=(high+j)/2;
+				}
+			}
+		} 
+		else
+		{
+			return false;
+		} 
+	}
+	return false;
+}
